ab238db39e2d36617b5d48b4a25e1d1c370df379
Jeremy Couthures <jeremy.couthures@lapp.in2p3.fr>
2023-09-18 15:18:48 +0200

    Improve code structure

--- Examples/Algorithms/Hashing/src/HashingAnnoy.cpp
+++ Examples/Algorithms/Hashing/src/HashingAnnoy.cpp
@@ -7,6 +7,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include "ActsExamples/Hashing/HashingAnnoy.hpp"
+#include "ActsExamples/Hashing/HashingTraining.hpp"
 
 #include "Acts/Definitions/Units.hpp"
 #include "ActsExamples/Hashing/kissrandom.h"
@@ -14,337 +15,169 @@
 
 #include <map>
 #include <vector>
+#include <set>
 
-
-ActsExamples::ProcessCode ActsExamples::HashingAnnoy::ComputeHitBuckets(
-    const AlgorithmContext& ctx, 
-    const ActsExamples::SimHitContainer& hits,
-    const unsigned int bucketSize) {
-  using Scalar = Acts::ActsScalar;
-  //if (not m_outputFile) {
-  //  ACTS_ERROR("Missing output file");
-  //  return ProcessCode::ABORT;
-  //}
-
-  // ensure exclusive access to tree/file while writing
-  //std::lock_guard<std::mutex> lock(m_writeMutex);
-
-  int32_t f = 2; // add it as arg; corresponds to the number of features
-  int hitIndex = 0;
-  //-DANNOYLIB_MULTITHREADED_BUILD
-  //Annoy::AnnoyIndex<int, double, Annoy::Angular, Annoy::Kiss32Random, Annoy::AnnoyIndexMultiThreadedBuildPolicy> t = 
-  //Annoy::AnnoyIndex<int, double, Annoy::Angular, Annoy::Kiss32Random, Annoy::AnnoyIndexMultiThreadedBuildPolicy>(f);
-
-  Annoy::AnnoyIndex<int, double, Annoy::Angular, Annoy::Kiss32Random, 
-                    Annoy::AnnoyIndexSingleThreadedBuildPolicy> t = 
-  Annoy::AnnoyIndex<int, double, Annoy::Angular, Annoy::Kiss32Random, 
-                    Annoy::AnnoyIndexSingleThreadedBuildPolicy>(f);
-
-
-  // Get the event number
-  int32_t eventId = ctx.eventNumber;
-  
-  // Add hit parameters to Annoy
-  for (const auto& hit : hits) {
-    //int32_t particleId = hit.particleId().value();
-    //int32_t geometryId = hit.geometryId().value();
-    // write hit position
-    Scalar tx = hit.fourPosition().x() / Acts::UnitConstants::mm;
-    Scalar ty = hit.fourPosition().y() / Acts::UnitConstants::mm;
-    Scalar tz = hit.fourPosition().z() / Acts::UnitConstants::mm;
-    Scalar tt = hit.fourPosition().w() / Acts::UnitConstants::ns;
-    // write four-momentum before interaction
-    //Scalar tpx = hit.momentum4Before().x() / Acts::UnitConstants::GeV;
-    //Scalar tpy = hit.momentum4Before().y() / Acts::UnitConstants::GeV;
-    //Scalar tpz = hit.momentum4Before().z() / Acts::UnitConstants::GeV;
-    //Scalar te = hit.momentum4Before().w() / Acts::UnitConstants::GeV;
-    // write four-momentum change due to interaction
-    //const auto delta4 = hit.momentum4After() - hit.momentum4Before();
-    //Scalar deltapx = delta4.x() / Acts::UnitConstants::GeV;
-    //Scalar deltapy = delta4.y() / Acts::UnitConstants::GeV;
-    //Scalar deltapz = delta4.z() / Acts::UnitConstants::GeV;
-    //Scalar deltae = delta4.w() / Acts::UnitConstants::GeV;
-    // write hit index along trajectory
-    //int32_t index = hit.index();
-    // decoded geometry for simplicity
-    /*
-    m_volumeId = hit.geometryId().volume();
-    m_boundaryId = hit.geometryId().boundary();
-    m_layerId = hit.geometryId().layer();
-    m_approachId = hit.geometryId().approach();
-    m_sensitiveId = hit.geometryId().sensitive();
-    */
-    
-    /*if (0 >= f) {
-      ACTS_ERROR("f must be positive");
-      return ProcessCode::ABORT;
-    }*/
-    double *vec = (double *) malloc( f * sizeof(double) );
-    vec[0] = (double)tx;
-    if (f >= 2){
-        vec[1] = (double)ty;
-    }
-    if (f >= 3){
-        vec[2] = (double)tz;
-    }
-    
-    t.add_item(hitIndex, vec);
-    
-    // Fill the tree
-    //m_outputTree->Fill();
-    /**
-    if (hitIndex == index){
-      std::cout << "Hit " << index << " same index" << std::endl;
-    } else {
-      std::cout << "Hit " << index << " different hit index " << hitIndex << std::endl;
-    }
-
-    if ((hit.fourPosition() == hits.nth(hitIndex)->fourPosition()) && 0){
-      std::cout << "Hit " << hitIndex << " same hit " << std::endl;
-    } else {
-      std::cout << "Hit " << hitIndex << " different hit " << std::endl;
-      //int hitIndex2 = 0;
-      for (auto h = hits.begin(); h != hits.end(); ++h) {
-        //const auto& simHit = *h;
-        const auto hitIndex2 = hits.index_of(h);
-        if (hit.fourPosition() == h->fourPosition()){
-          std::cout << "Hit " << hitIndex << " same hit " << hitIndex2 << std::endl;
-          break;
-        }
-      }
-    }
-    //**/
-    hitIndex++;
-  }
-
-  if ((unsigned int)hitIndex != hits.size()){
-    //ACTS_ERROR("Not the same number of buckets than the number of hits");
-    std::cout << "Not the same number of buckets than the number of hits" << std::endl;
-    return ActsExamples::ProcessCode::ABORT;
-  }
-
-  unsigned int n_trees = 2 * f;
-  
-  t.build(n_trees);
-  
-  //std::cout << "Bucket Size " << bucketSize << std::endl;
-  //std::cout << "Saving index ..." << std::endl;
-	//t.save("precision.tree");
-	//std::cout << " Done" << std::endl;
-
-
-
-	//******************************************************
-
-	// doing the work
-	for(hitIndex=0; hitIndex < hits.size(); hitIndex++){
-		//std::cout << "finding neighbours for hit " << hitIndex << " event " << eventId << std::endl;
-
-	  SimHitContainer::sequence_type bucket;
-	  std::vector<int> bucket_ids;
-
-		// get the bucketSize closests
-		t.get_nns_by_item(hitIndex, bucketSize, -1, &bucket_ids, nullptr); //search_k defaults to "n_trees * n" if not provided.
-
-    std::sort(bucket_ids.begin(), bucket_ids.end(), std::less<int>());
-    for(const auto& bucketHitidx : bucket_ids){
-        bucket.push_back(*(hits.nth(bucketHitidx)));
-		}
-
-    // associate hit index with the bucket
-		m_bucketsMap[hitIndex] = bucket;
-    
-    /**
-    // write simulated hits
-    CsvSimHitWriter::Config writeSimHits;
-    writeSimHits.inputSimHits = "hashingBucket" + to_string(eventId) + "_" + to_string (i);
-    writeSimHits.outputDir = "./csv"//outputDir;
-    writeSimHits.outputStem = "hits";
-    // issue: no sequencer
-    sequencer.addWriter(
-        std::make_shared<CsvSimHitWriter>(writeSimHits, logLevel));
-    **/
-
-	}
-
-	//std::cout << "\nDone" << std::endl;
-
-  return ActsExamples::ProcessCode::SUCCESS;
+bool LayerSelection(double r2, double z){
+  bool isInside = (r2 > 25*25 && r2 < 40*40) && (z > -550 && z < 550);
+  // if ((r2 < 25*25 || r2 > 40*40 || (z < -550 || z > 550))
+      //     && (r2 < 25*25 || r2 > 190*190 || z < -630 || z > -550)
+      //     && (r2 < 25*25 || r2 > 190*190 || z < 550 || z > 630)){
+  return isInside;
 }
 
+int GetBinIndex(double r2, double z, unsigned int zBins){
+  using Scalar = Acts::ActsScalar;
+  Scalar binSize = 1100.0/zBins;
+  int binIndex = (z - (-550) + 0.5*binSize)/binSize;
+  // int binIndex = (z - (-550))/binSize;
+  // Scalar z = 1100.0/zBins*binIndex + -550;
+  return binIndex;
+}
+
+int GetBinIndexPhi(double phi, unsigned int phiBins){
+  using Scalar = Acts::ActsScalar;
+  Scalar binSize = 2*M_PI/phiBins;
+  int binIndex = (phi+M_PI)/binSize;
+  return binIndex;
+}
+
+// template <typename AnnoyModel>
 ActsExamples::ProcessCode ActsExamples::HashingAnnoy::ComputeSpacePointsBuckets(
     const AlgorithmContext& ctx, 
+    // AnnoyModel* annoyModel,
+    const Annoy::AnnoyIndex<unsigned int, double, Annoy::AngularEuclidean, Annoy::Kiss32Random, 
+                    Annoy::AnnoyIndexSingleThreadedBuildPolicy>* annoyModel,
     const ActsExamples::SimSpacePointContainer& spacePoints,
-    const unsigned int bucketSize) {
+    const unsigned int bucketSize,
+    const unsigned int zBins,
+    const unsigned int phiBins) {
   using Scalar = Acts::ActsScalar;
 
-  int32_t f = 2; // add it as arg; corresponds to the number of features
-  int spacePointIndex = 0;
-  using AnnoyMetric = Annoy::Angular;
-  //using AnnoyMetric = Annoy::Euclidean;
-  //-DANNOYLIB_MULTITHREADED_BUILD
-  //Annoy::AnnoyIndex<int, double, Annoy::Angular, Annoy::Kiss32Random, Annoy::AnnoyIndexMultiThreadedBuildPolicy> t = 
-  //Annoy::AnnoyIndex<int, double, Annoy::Angular, Annoy::Kiss32Random, Annoy::AnnoyIndexMultiThreadedBuildPolicy>(f);
-
-  Annoy::AnnoyIndex<int, double, AnnoyMetric, Annoy::Kiss32Random, 
-                    Annoy::AnnoyIndexSingleThreadedBuildPolicy> t = 
-  Annoy::AnnoyIndex<int, double, AnnoyMetric, Annoy::Kiss32Random, 
-                    Annoy::AnnoyIndexSingleThreadedBuildPolicy>(f);
-
-
-  // Get the event number
-  int32_t eventId = ctx.eventNumber;
-  
-  // Add spacePoints parameters to Annoy
-  for (const auto& spacePoint : spacePoints) {
-    //int32_t geometryId = hit.geometryId().value();
-    // write spacePoint position
-    Scalar x = spacePoint.x() / Acts::UnitConstants::mm;
-    Scalar y = spacePoint.y() / Acts::UnitConstants::mm;
-    Scalar z = spacePoint.z() / Acts::UnitConstants::mm;
-    //Scalar tt = hit.fourPosition().w() / Acts::UnitConstants::ns;
-
-    // Helix transform
-    Scalar r2 = x*x + y*y;
-    // if (r2 > 200*200){
-    // 	continue;
-    // }
-    //x = x / r2;
-    //y = y / r2;
-    /*if (0 >= f) {
-      ACTS_ERROR("f must be positive");
-      return ProcessCode::ABORT;
-    }*/
-    double *vec = (double *) malloc( f * sizeof(double) );
-    vec[0] = (double)x;
-    if (f >= 2){
-        vec[1] = (double)y;
-    }
-    if (f >= 3){
-        vec[2] = (double)z;
-    }
-    
-    t.add_item(spacePointIndex, vec);
-    spacePointIndex++;
-  }
-
-  if ((unsigned int)spacePointIndex != spacePoints.size()){
-    //ACTS_ERROR("Not the same number of buckets than the number of hits");
-    std::cout << "Not the same number of buckets than the number of spacePoints" << std::endl;
-    return ActsExamples::ProcessCode::ABORT;
-  }
-
-  unsigned int n_trees = 2 * f;
-  
-  t.build(n_trees);
-  
-  //std::cout << "Bucket Size " << bucketSize << std::endl;
-  //std::cout << "Saving index ..." << std::endl;
-	//t.save("precision.tree");
-	//std::cout << " Done" << std::endl;
-
-
-
 	//******************************************************
+  // std::cout << annoyModel->get_seed() << "\n";
+  // std::cout << annoyModel->get_n_items() << "\n";
 
-  unsigned int n_buckets = 0;
+  if (zBins > 0){
+    std::set<ActsExamples::SimSpacePoint> bucketsSetSPMap[zBins];
+    for(unsigned int spacePointIndex=0; spacePointIndex < spacePoints.size(); spacePointIndex++){
+      auto spacePoint = spacePoints[spacePointIndex];
+      Scalar x = spacePoint.x() / Acts::UnitConstants::mm;
+      Scalar y = spacePoint.y() / Acts::UnitConstants::mm;
+      Scalar z = spacePoint.z() / Acts::UnitConstants::mm;
+      //Scalar tt = hit.fourPosition().w() / Acts::UnitConstants::ns;
 
-	// doing the work
-	for(spacePointIndex=0; spacePointIndex < spacePoints.size(); spacePointIndex++){
-		//std::cout << "finding neighbours for spacePoint " << spacePointIndex << " event " << eventId << std::endl;
-	  auto spacePoint = spacePoints[spacePointIndex];
-    Scalar x = spacePoint.x() / Acts::UnitConstants::mm;
-    Scalar y = spacePoint.y() / Acts::UnitConstants::mm;
-    Scalar z = spacePoint.z() / Acts::UnitConstants::mm;
-    //Scalar tt = hit.fourPosition().w() / Acts::UnitConstants::ns;
+      // Helix transform
+      Scalar r2 = x*x + y*y;
+      // std::cout << "r2:" << r2 << std::endl;
 
-	  SimSpacePointContainer bucket;
-    // Helix transform
-    // Scalar r2 = x*x + y*y;
-	  // std::cout << "r2:" << r2 << std::endl;
-    // if (r2 > 200*200){
-	  //   // std::cout << "r2 skip" << std::endl;
-    // 	continue;
-    // }
-    // if (r2 < 33*33){
-	  //   // std::cout << "r2 skip" << std::endl;
-		//   // m_bucketsSPMap[spacePointIndex] = bucket;
-    // 	continue;
-    // }
-    // if (r2 < 60*60){
-	  //   // std::cout << "r2 skip" << std::endl;
-		//   // m_bucketsSPMap[spacePointIndex] = bucket;
-    // 	continue;
-    // }
-    // if (r2 > 90*90){
-	  //   // std::cout << "r2 skip" << std::endl;
-		//   // m_bucketsSPMap[spacePointIndex] = bucket;
-    // 	continue;
-    // }
-    // if (r2 < 25*25){
-	  //   // std::cout << "r2 skip" << std::endl;
-		//   // m_bucketsSPMap[spacePointIndex] = bucket;
-    // 	continue;
-    // }
-    // if (r2 > 40*40){
-	  //   // std::cout << "r2 skip" << std::endl;
-		//   // m_bucketsSPMap[spacePointIndex] = bucket;
-    // 	continue;
-    // }
-	  // SimSpacePointContainer bucket;
-	  std::vector<int> bucket_ids;
-
-		// get the bucketSize closests spacePoints
-		t.get_nns_by_item(spacePointIndex, bucketSize, -1, &bucket_ids, nullptr); //search_k defaults to "n_trees * n" if not provided.
-
-    std::sort(bucket_ids.begin(), bucket_ids.end(), std::less<int>());
-    for(const auto& bucketSpacePointIndex : bucket_ids){
-        bucket.push_back(spacePoints.at(bucketSpacePointIndex));
-		}
-
-    bool found_same = false;
-    for(const auto& bucketSpacePointIndex : bucket_ids){
-        // std::cout << ",spacePointIndexPrev"  << spacePointIndexPrev << " spacePointIndex " << spacePointIndex << std::endl;
-        if (m_bucketsSPMap.find(bucketSpacePointIndex) != m_bucketsSPMap.end()){
-          SimSpacePointContainer prev_bucket = m_bucketsSPMap.find(bucketSpacePointIndex)->second;
-          if (bucket == prev_bucket){
-            found_same = true;
-            // std::cout << "found_same" << std::endl;
-            break;
-          }
-        }
+      if (!LayerSelection(r2, z)){
+        // std::cout << "r2 skip" << std::endl;
+        // m_bucketsSPMap[spacePointIndex] = bucket;
+        continue;
       }
-    // if (spacePointIndex > 0){
-    //   if (found_same){
-    //     continue;
-    //   }
-    //   for (unsigned int spacePointIndexPrev = spacePointIndex-1; spacePointIndexPrev >= 0; spacePointIndexPrev--){
-    //     // std::cout << ",spacePointIndexPrev"  << spacePointIndexPrev << " spacePointIndex " << spacePointIndex << std::endl;
-    //     if (m_bucketsSPMap.find(spacePointIndexPrev) != m_bucketsSPMap.end()){
-    //       SimSpacePointContainer prev_bucket = m_bucketsSPMap.find(spacePointIndexPrev)->second;
-    //       if (bucket == prev_bucket){
-    //         found_same = true;
-    //         std::cout << "bug found_same" << std::endl;
-    //         std::cout << ",spacePointIndexPrev"  << spacePointIndexPrev << " spacePointIndex " << spacePointIndex << std::endl;
-    //         break;
-    //       }
-    //     }
-    //     if (spacePointIndexPrev == 0){
-    //       break;
-    //     }
-    //   }
-    // }
-    if (found_same){
-      continue;
+
+      int binIndex = GetBinIndex(r2, z, zBins);
+      if (binIndex < 0 || binIndex >= zBins){
+        // ACTS_ERROR("binIndex outside of bins covering");
+        std::cout << "binIndex outside of bins covering" << std::endl;
+        return ActsExamples::ProcessCode::ABORT;
+      }
+      
+      // std::cout << "Here1\n";
+      std::set<ActsExamples::SimSpacePoint> *bucketSet;
+      bucketSet = &bucketsSetSPMap[binIndex];
+
+      // SimSpacePointContainer bucket;
+      std::vector<unsigned int> bucket_ids;
+
+      /// Get the bucketSize closests spacePoints
+      annoyModel->get_nns_by_item(spacePointIndex, bucketSize, -1, &bucket_ids, nullptr);
+
+      // Scalar phi = atan2(y, x);
+      // double *vec = (double *) malloc( 1 * sizeof(double) );
+      // vec[0] = (double)phi;
+      // annoyModel->get_nns_by_vector(vec, bucketSize, -1, &bucket_ids, nullptr);
+
+      if (bucketSize != bucket_ids.size()){
+         std::cout << "bucketSize: " << bucketSize << " returned: " << bucket_ids.size() << "\n";
+      }
+      for(const auto& bucketSpacePointIndex : bucket_ids){
+          bucketSet->insert(spacePoints.at(bucketSpacePointIndex));
+      }
+      
+      // m_bucketsSPMap[binIndex] = bucketSet;
     }
 
-    // associate spacePoint index with the bucket
-		m_bucketsSPMap[spacePointIndex] = bucket;
-    n_buckets++;
-	}
+    unsigned int n_buckets = 0;
+    for (int binIndex = 0; binIndex < zBins; binIndex++){
+      if (bucketsSetSPMap[binIndex].size() > 0){
+        m_bucketsSPMap[n_buckets] = bucketsSetSPMap[binIndex];
+        n_buckets++;
+      }
+    }
+  } else if (phiBins > 0) {
+    std::set<ActsExamples::SimSpacePoint> bucketsSetSPMap[phiBins];
+    for(unsigned int spacePointIndex=0; spacePointIndex < spacePoints.size(); spacePointIndex++){
+      auto spacePoint = spacePoints[spacePointIndex];
+      Scalar x = spacePoint.x() / Acts::UnitConstants::mm;
+      Scalar y = spacePoint.y() / Acts::UnitConstants::mm;
+      Scalar z = spacePoint.z() / Acts::UnitConstants::mm;
+      //Scalar tt = hit.fourPosition().w() / Acts::UnitConstants::ns;
 
-	//std::cout << "\nDone" << std::endl;
-	std::cout << "n_buckets:" << n_buckets << std::endl;
+      // Helix transform
+      Scalar r2 = x*x + y*y;
+      // std::cout << "r2:" << r2 << std::endl;
+
+      if (!LayerSelection(r2, z)){
+        // std::cout << "r2 skip" << std::endl;
+        // m_bucketsSPMap[spacePointIndex] = bucket;
+        continue;
+      }
+
+      Scalar phi = atan2(y, x);
+
+      int binIndex = GetBinIndexPhi(phi, phiBins);
+      if (binIndex < 0 || binIndex >= phiBins){
+        // ACTS_ERROR("binIndex outside of bins covering");
+        std::cout << "binIndex outside of bins covering" << std::endl;
+        return ActsExamples::ProcessCode::ABORT;
+      }
+      
+      // std::cout << "Here1\n";
+      std::set<ActsExamples::SimSpacePoint> *bucketSet;
+      bucketSet = &bucketsSetSPMap[binIndex];
+
+      // SimSpacePointContainer bucket;
+      std::vector<unsigned int> bucket_ids;
+
+      /// Get the bucketSize closests spacePoints
+      annoyModel->get_nns_by_item(spacePointIndex, bucketSize, -1, &bucket_ids, nullptr);
+
+      // Scalar phi = atan2(y, x);
+      // double *vec = (double *) malloc( 1 * sizeof(double) );
+      // vec[0] = (double)phi;
+      // annoyModel->get_nns_by_vector(vec, bucketSize, -1, &bucket_ids, nullptr);
+
+      if (bucketSize != bucket_ids.size()){
+         std::cout << "bucketSize: " << bucketSize << " returned: " << bucket_ids.size() << "\n";
+      }
+      for(const auto& bucketSpacePointIndex : bucket_ids){
+          bucketSet->insert(spacePoints.at(bucketSpacePointIndex));
+      }
+      
+      // m_bucketsSPMap[binIndex] = bucketSet;
+    }
+
+    unsigned int n_buckets = 0;
+    for (int binIndex = 0; binIndex < phiBins; binIndex++){
+      if (bucketsSetSPMap[binIndex].size() > 0){
+        m_bucketsSPMap[n_buckets] = bucketsSetSPMap[binIndex];
+        n_buckets++;
+      }
+    }
+  }
+  else {
+  }
 
   return ActsExamples::ProcessCode::SUCCESS;
 }
